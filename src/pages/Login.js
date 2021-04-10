71d05bbed75e0e157e8e3b7bcde418eeecf3e545
seongwookchun <w2da130@uos.ac.kr>
2021-04-10 05:44:09 +0000

    tooltip(css) prototype

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Login.css';
 
 let APP_NAME = '멍냥이의 하루';
 let PLACEHOLDER_ID = '';//📧이메일을 입력해주세요.';
@@ -7,20 +7,20 @@ let LABEL_EMAIL = '이메일';
 let LABEL_PW = '비밀번호';
 let BTN_LOGIN = '로그인';
 let LABEL_FIND_IDPW = '아이디 / 비밀번호 찾기';
-
+let TOOLTIP_FIND_IDPW = '아이디 / 비밀번호 찾기';
 const Login = () => {
 	return (
 		<div class="Page-login">
 			<h1>login</h1>
 			<div class="card-area app-logo">
 				<dl>
-					<dt class="card-title">{APP_NAME}</dt>
+					<dt class="card-title">{APP_NAME}</dt>					
 					<dd></dd>
 				</dl>
 			</div>
 			<form action="">
 				<div class="input-area">
-					<label for="id">{LABEL_EMAIL}</label>
+					<label for="id" data-tool-tip="로그인 아이디로 이메일 주소를 입력해주세요.">{LABEL_EMAIL}</label>
 					<input type="text" placeholder={PLACEHOLDER_ID}></input>
 				</div>
 				<div class="input-area">
@@ -31,10 +31,14 @@ const Login = () => {
 					<button type="submit" class="btn-normal-medium">{BTN_LOGIN}</button>
 				</div>
 				<div class="article-area">
-					{LABEL_FIND_IDPW}
+					<a data-tool-tip={TOOLTIP_FIND_IDPW} href="#">{LABEL_FIND_IDPW}</a>
 				</div>
 				
 			</form>
+			<p>
+				Lorem ipsum <a href="#" data-tool-tip="tool tip test">mouse over on it!.</a>
+				great.
+			</p>
 		</div>
 	)
 }
